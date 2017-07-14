af28abeb2e581ac13639f454e8976b41f32804d5
李炎熹 <liyanxi@liyanxideMacBook-Pro.local>
2017-07-14 14:56:28 +0800

    add log

--- app/controller/graph/graph_controller.go
+++ app/controller/graph/graph_controller.go
@@ -32,7 +32,8 @@ func EndpointObjGet(c *gin.Context) {
 	inputs := APIEndpointObjGetInputs{
 		Deadline: 0,
 	}
-	log.Println("inputs:" + inputs)
+	log.Println("inputs:")
+	log.Println(inputs)
 	if err := c.Bind(&inputs); err != nil {
 		h.JSONR(c, badstatus, err)
 		return
@@ -47,7 +48,8 @@ func EndpointObjGet(c *gin.Context) {
 		Where("endpoint in (?) and ts >= ?", inputs.Endpoints, inputs.Deadline).
 		Scan(&result)
 
-	log.Println("dt:==" + dt)
+	log.Println("dt")
+	log.Println(dt)
 	if dt.Error != nil {
 		h.JSONR(c, http.StatusBadRequest, dt.Error)
 		return
@@ -58,8 +60,12 @@ func EndpointObjGet(c *gin.Context) {
 		endpoints = append(endpoints, map[string]interface{}{"id": r.ID, "endpoint": r.Endpoint, "ts": r.Ts})
 	}
 
-	log.Println("result,end==>" + result + endpoints)
-	log.Println("c===?" *c)
+	log.Println("result,end==>")
+	log.Println(result)
+	log.Println(endpoints)
+
+	log.Println("c===?")
+	log.Println(*c)
 	h.JSONR(c, endpoints)
 
 }
